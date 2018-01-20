texjporg/tex-jp-build

commit d662abbe3e9107b599c350cc9434bed691b95249

ptex-base.ch: added 'not is_char_node(tail)' guard

source/texk/web2c/ptexdir/ptex-base.ch
@@ -7152,7 +7152,7 @@ end;
 @ @<Look ahead for glue or kerning@>=
 cur_q:=tail;
 if inhibit_glue_flag<>true then
-  begin if (tail=link(head))and(type(tail)=disp_node) then
+  begin if (tail=link(head))and(not is_char_node(tail))and(type(tail)=disp_node) then
     goto skip_loop
   else begin if char_tag(main_i)=gk_tag then
     begin main_k:=glue_kern_start(main_f)(main_i);

commit f23c7259d64f443983f95198dd0867ccd9f6610d

fix a regression by d662abb

source/texk/web2c/ptexdir/ptex-base.ch
@@ -4384,7 +4384,7 @@ r:=link(q); link(q):=null; q:=link(temp_head); link(temp_head):=r;
 if last_disp<>0 then begin
   r:=get_node(small_node_size);
   type(r):=disp_node; disp_dimen(r):=last_disp;
-  link(r):=q; q:=r;
+  link(r):=q; q:=r; disp_called:=true;
   end;
 @z
 
@@ -7146,6 +7146,7 @@ begin if not is_char_node(tail)and(type(tail)=disp_node) then
 else
   begin prev_node:=tail; tail_append(get_node(small_node_size));
   type(tail):=disp_node; disp_dimen(tail):=0; prev_disp:=disp;
+  disp_called:=true
   end;
 end;
 