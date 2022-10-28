d5f7ac3ec9594d6b419021367fab1132e515c2f9
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2022-10-28 21:14:34 +0900

    nptexdir: cleanup, eup/e-up -> np

--- source/texk/web2c/nptexdir/nptexextra.c
+++ source/texk/web2c/nptexdir/nptexextra.c
@@ -5,7 +5,7 @@
 #define	EXTERN /* Instantiate data from nptexd.h here.  */
 #define DLLPROC dllnptexmain
 
-/* This file defines TeX and eupTeX.  */
+/* This file defines TeX and npTeX.  */
 #include <nptexd.h>
 
 /* Hand-coded routines for TeX or Metafont in C.  */
--- source/texk/web2c/nptexdir/nptexextra.h
+++ source/texk/web2c/nptexdir/nptexextra.h
@@ -1,13 +1,13 @@
 /* nptexextra.h: banner etc. for npTeX.
 
-   This is included by npTeX, from euptexextra.c
+   This is included by npTeX, from nptexextra.c
 */
 
 #include <etexdir/etex_version.h> /* for ETEX_VERSION */
 
 #define BANNER "This is npTeX, Version 3.141592653-" ETEX_VERSION "-np0.0.00"
 #define COPYRIGHT_HOLDER "D.E. Knuth"
-#define AUTHOR "Peter Breitenlohner"
+#define AUTHOR "Japanese TeX Development Community"
 #define PROGRAM_HELP NPTEXHELP
 #define BUG_ADDRESS "issue@texjp.org"
 #define DUMP_VAR TEXformatdefault
